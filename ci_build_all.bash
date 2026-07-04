b9857f666e113498493c9f95be8a090af74bd83c
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-07-05 00:46:13 +0300

    fix[lwjgl]: apply the 3.2.3 patch only to 3.2.3

--- ci_build_all.bash
+++ ci_build_all.bash
@@ -58,16 +58,16 @@ fi
 if [[ "$LWJGL_VERSION" == "3.3.1" ]]; then
    apply_patch lwjgl3_3.3.1_xxhash_static_assert "fix static assert macro in xxHash"
    apply_patch lwjgl3_3.3.1_uring_type "add missing type into ui_uring.h"
-   apply_patch lwjgl3_3.3.1_replace_locale-sensitive_string_format "fix crash on arabic locale by replacing locale-sensitive String.format"
+   apply_patch lwjgl3_3.3.1_replace_locale-sensitive_string_format "[3.3.1] fix crash on arabic locale by replacing locale-sensitive String.format"
 fi
 
 if [[ "$LWJGL_VERSION" == "3.2.3" || "$LWJGL_VERSION" == "3.3.1" ]]; then
    apply_patch lwjgl3_3.2.3_use_packaging_arch "fix offline build natives arch"
-   apply_patch lwjgl3_3.2.3_replace_locale-sensitive_string_format "fix crash on arabic locale by replacing locale-sensitive String.format"
 fi
 
 if [[ "$LWJGL_VERSION" == "3.2.3" ]]; then
    apply_patch lwjgl3_3.2.3_rpmalloc_madvise "use linux madvise instead of posix_madvise when available"
+   apply_patch lwjgl3_3.2.3_replace_locale-sensitive_string_format "[3.2.3] fix crash on arabic locale by replacing locale-sensitive String.format"
 fi
 
 export ANTFLAGS="-lib $NASHORN -Dplatform.linux=true -Dbinding.nfd=false -Dbinding.jawt=false -Dbinding.remotery=false -Dbinding.zstd=false -Dbinding.yoga=false -Dbinding.meow=false"
